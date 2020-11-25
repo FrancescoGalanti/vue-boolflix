6aace4a10c562af8ad5f121707bdecf27d27e94d
FrancescoGalanti <galanti_francesco>
2020-11-25 16:05:02 +0100

    inizio funzione

--- javascript/main.js
+++ javascript/main.js
@@ -12,19 +12,23 @@ var app = new Vue({
    },
 
 
-
-
-
    methods:{
+         // Function that allow to filter the movie per titlte using query
+         searchingMovie(){
 
+           const self = this;
+            // calling the API with Jason //
+            axios.get('https://api.themoviedb.org/3/search/movie?', {
+                  params:{
+                    api_key: 'e99307154c6dfb0b4750f6603256716d',
+                    query: self.movieSearched
+                  }
+            })
 
-
-
-       }
-
+          }
+        }      
 
 
 
 
 });
-.
