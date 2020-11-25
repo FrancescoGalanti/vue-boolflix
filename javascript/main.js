a7565b94d1c29427e2579657d6c4409f7e10ea86
FrancescoGalanti <galanti_francesco>
2020-11-25 22:01:27 +0100

    working

--- javascript/main.js
+++ javascript/main.js
@@ -11,34 +11,54 @@ var app = new Vue({
      movieSearched: "",
    },
 
+   created(){
 
-   methods:{
-         // Function that allow to filter the movie per titlte using query
-         searchingMovie(){
+     axios.get('https://api.themoviedb.org/3/search/movie?', {
+           params:{
+             api_key: 'e99307154c6dfb0b4750f6603256716d',
+             query: 'fantozzi',
+             language: 'it-IT',
+           }
 
+          )}
 
-            // calling the API with Jason //
-            axios.get('https://api.themoviedb.org/3/search/movie', {
-                  params:{
-                    api_key: 'e99307154c6dfb0b4750f6603256716d',
-                    query: this.movieSearched,
-                    language: 'it-IT',
-                  }
-               })
+           .then(response => {
+             // default situation that leads to not printing any element if the string is empty
+              console.log(response)
+            )}
 
-                .then(response =>{
-                  // default situation that leads to not printing any element if the string is empty 
-                  if (this.searchKey.trim() != '') {
-                   this.listaFiltered = result.data.results;
-                  }
-
-                 })
-                .catch(error =>{
-                 console.log(error);
-                });
-
-            }
-        }
+           .catch(error =>{
+            console.log(error);
+           });
 
 
-});
+       },
+  });
+
+   // methods:{
+   //       // Function that allow to filter the movie per titlte using query
+   //       searchingMovie(){
+   //
+   //
+   //          // calling the API with Jason //
+   //          axios.get('https://api.themoviedb.org/3/search/movie?', {
+   //                params:{
+   //                  api_key: 'e99307154c6dfb0b4750f6603256716d',
+   //                  query: this.movieSearched,
+   //                  language: 'it-IT',
+   //                }
+   //             })
+   //
+   //              .then(response =>{
+   //                // default situation that leads to not printing any element if the string is empty
+   //                if (this.searchKey.trim() != '') {
+   //                 this.listaFiltered = result.data.results;
+   //                }
+   //
+   //               })
+   //              .catch(error =>{
+   //               console.log(error);
+   //              });
+   //
+   //          }
+   //      }
