4ef163f0c17016cdc3a0dc2abbf48475a07374d9
FrancescoGalanti <galanti_francesco>
2020-11-26 07:01:25 +0100

    calling

--- javascript/main.js
+++ javascript/main.js
@@ -14,51 +14,23 @@ var app = new Vue({
    created(){
 
      axios.get('https://api.themoviedb.org/3/search/movie?', {
-           params:{
+           params: {
              api_key: 'e99307154c6dfb0b4750f6603256716d',
              query: 'fantozzi',
              language: 'it-IT',
-           }
+            }
+         })
 
-          )}
 
            .then(response => {
              // default situation that leads to not printing any element if the string is empty
               console.log(response)
-            )}
+            })
 
-           .catch(error =>{
+           .catch(error => {
             console.log(error);
            });
 
 
        },
   });
-
-   // methods:{
-   //       // Function that allow to filter the movie per titlte using query
-   //       searchingMovie(){
-   //
-   //
-   //          // calling the API with Jason //
-   //          axios.get('https://api.themoviedb.org/3/search/movie?', {
-   //                params:{
-   //                  api_key: 'e99307154c6dfb0b4750f6603256716d',
-   //                  query: this.movieSearched,
-   //                  language: 'it-IT',
-   //                }
-   //             })
-   //
-   //              .then(response =>{
-   //                // default situation that leads to not printing any element if the string is empty
-   //                if (this.searchKey.trim() != '') {
-   //                 this.listaFiltered = result.data.results;
-   //                }
-   //
-   //               })
-   //              .catch(error =>{
-   //               console.log(error);
-   //              });
-   //
-   //          }
-   //      }
